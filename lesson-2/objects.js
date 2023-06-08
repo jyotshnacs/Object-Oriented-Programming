1d07f550d5804a90363d4d6eaaa103de22be28ae
jo <jyotshnacs@gmail.com>
2023-06-08 11:37:38 +0100

    prop

--- lesson-2/objects.js
+++ lesson-2/objects.js
@@ -1,4 +1,4 @@
-let userOne = {
+var userOne = {
   email: "ryu@ninjas.com",
   name: "Ryu",
   login() {
@@ -9,5 +9,19 @@ let userOne = {
   },
 };
 
+var prop = "name";
+
+// userOne.email = 'ryu2k18@ninjas.com';
+// userOne['email'] = 'ryu2k18@ninjas.com';
+// userOne[prop] = 'Ryuke';
+
+// console.log(userOne);
+
+userOne.age = 25;
+userOne.logInfo = function () {
+  console.log(this.name, this.age, this.email);
+};
+
+userOne.logInfo();
 userOne.login();
 userOne.logout();
