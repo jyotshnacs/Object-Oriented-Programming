88dc94da3e82fb8e7a616f705dbf1e8881b925d9
jo <jyotshnacs@gmail.com>
2023-06-09 14:54:06 +0100

    __proto__

--- lesson-2/objects.js
+++ lesson-2/objects.js
@@ -80,10 +80,17 @@ function User(email, name) {
   this.email = email;
   this.name = name;
   this.online = false;
-  this.login = function () {
-    console.log(this.email, "has logged in");
-  };
 }
+
+User.prototype.login = function () {
+  this.online = true;
+  console.log(this.email, "has logged in");
+};
+
+User.prototype.logout = function () {
+  this.online = false;
+  console.log(this.email, "has logged out");
+};
 
 var userOne = new User("scream@blahblah.com", "scream");
 var userTwo = new User("calm@down.com", "calm");
